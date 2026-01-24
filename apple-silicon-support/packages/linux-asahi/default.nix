4a41ff868efcd300be50bb5c38b8d09060f4de2c
Slendi <slendi@socopon.com>
2026-01-25 01:06:38 +0200

    oops
    Signed-off-by: Slendi <slendi@socopon.com>

--- apple-silicon-support/packages/linux-asahi/default.nix
+++ apple-silicon-support/packages/linux-asahi/default.nix
@@ -18,7 +18,7 @@ let
       inherit stdenv lib;
 
       pname = "linux-asahi";
-      version = "6.18.6";
+      version = "6.18.5";
       modDirVersion = version;
       extraMeta.branch = "6.18";
 
