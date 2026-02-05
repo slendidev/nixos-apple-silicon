b55e83aac1987e13d7fdde63da73fb74b055aab1
Slendi <slendi@socopon.com>
2026-02-05 23:37:34 +0200

    help
    Signed-off-by: Slendi <slendi@socopon.com>

--- apple-silicon-support/packages/linux-asahi/default.nix
+++ apple-silicon-support/packages/linux-asahi/default.nix
@@ -32,33 +32,33 @@ let
       ignoreConfigErrors = true;
       configfile = ./config;
 
-      # kernelPatches = [
-      #   {
-      #     name = "Asahi config";
-      #     patch = null;
-      #     structuredExtraConfig = with lib.kernel; {
-      #       # Needed for GPU
-      #       ARM64_16K_PAGES = yes;
+      kernelPatches = [
+        {
+          name = "Asahi config";
+          patch = null;
+          structuredExtraConfig = with lib.kernel; {
+            # Needed for GPU
+            ARM64_16K_PAGES = yes;
 
-      #       ARM64_MEMORY_MODEL_CONTROL = yes;
-      #       ARM64_ACTLR_STATE = yes;
+            ARM64_MEMORY_MODEL_CONTROL = yes;
+            ARM64_ACTLR_STATE = yes;
 
-      #       # Might lead to the machine rebooting if not loaded soon enough
-      #       APPLE_WATCHDOG = yes;
+            # Might lead to the machine rebooting if not loaded soon enough
+            APPLE_WATCHDOG = yes;
 
-      #       # Can not be built as a module, defaults to no
-      #       APPLE_M1_CPU_PMU = yes;
+            # Can not be built as a module, defaults to no
+            APPLE_M1_CPU_PMU = yes;
 
-      #       # Defaults to 'y', but we want to allow the user to set options in modprobe.d
-      #       HID_APPLE = module;
+            # Defaults to 'y', but we want to allow the user to set options in modprobe.d
+            HID_APPLE = module;
 
-      #       APPLE_PMGR_MISC = yes;
-      #       APPLE_PMGR_PWRSTATE = yes;
-      #     };
-      #     features.rust = true;
-      #   }
-      # ]
-      # ++ _kernelPatches;
+            APPLE_PMGR_MISC = yes;
+            APPLE_PMGR_PWRSTATE = yes;
+          };
+          features.rust = true;
+        }
+      ]
+      ++ _kernelPatches;
     };
 
   linux-asahi = callPackage linux-asahi-pkg { };
